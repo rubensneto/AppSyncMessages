89fa8bb71ff7ee80d64fc7488229380c17a19a00
Rubens Neto <>
2018-06-25 13:09:05 +0100

    Shapping CollectionView

--- AppSyncMessages/AppSyncMessages/ConversationsController.swift
+++ AppSyncMessages/AppSyncMessages/ConversationsController.swift
@@ -8,13 +8,83 @@
 
 import UIKit
 
-class ConversationsController: UICollectionViewController {
+class ConversationsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .black
+        collectionView?.register(ConversationCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.alwaysBounceVertical = true
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 20, height: 70)
+    }
+}
+
+class ConversationCell: T101Cell {
+    
+    let onlineIndicatorView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 25
+        view.layer.masksToBounds = true
+        view.backgroundColor = UIColor(red: 0.01, green: 0.87, blue: 0, alpha: 1)
+        return view
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 23
+        imageView.layer.masksToBounds = true
+        imageView.backgroundColor = .red
+        imageView.layer.borderColor = UIColor(red: 0.16, green: 0.16, blue: 0.16, alpha: 1).cgColor
+        imageView.layer.borderWidth = 2
+        return imageView
+    }()
+    
+    override func setupView(){
+        self.layer.cornerRadius = 10
+        backgroundColor = UIColor(red: 0.16, green: 0.16, blue: 0.16, alpha: 1)
+        
+        addSubview(onlineIndicatorView)
+        onlineIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[v0(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": onlineIndicatorView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[v0(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": onlineIndicatorView]))
+        
+        addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-12-[v1(46)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v1": profileImageView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-12-[v1(46)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v1": profileImageView]))
+        
+       
+    }
+    
+}
+
+class T101Cell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        backgroundColor = .darkGray
+    }
 }
 
