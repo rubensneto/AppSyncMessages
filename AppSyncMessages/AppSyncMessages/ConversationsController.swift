619f6c841819af0e69b5ec966e78409460312ebb
Rubens Neto <>
2018-06-25 12:09:31 +0100

    Initial configurations

---
+++ AppSyncMessages/AppSyncMessages/ConversationsController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppSyncMessages
+//
+//  Created by Rubens Neto on 25/06/2018.
+//  Copyright © 2018 T101. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .red
+    }
+
+}
+
